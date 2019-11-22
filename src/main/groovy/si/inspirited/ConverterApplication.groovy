4726e15d2207b7ddb6f5502827e406c1ac83ebdb
JavaEEPRO <j2eengineer@gmail.com>
2019-11-22 10:45:56 +0200

    implemented csv parser

--- src/main/groovy/si/inspirited/ConverterApplication.groovy
+++ src/main/groovy/si/inspirited/ConverterApplication.groovy
@@ -13,6 +13,14 @@ class ConverterApplication implements CommandLineRunner{
 
 	@Override
 	void run(String... args) throws Exception {
-
+		String FILE_TO_PARSE_PATH = "D:/input.csv"
+		if (args.length > 0) { FILE_TO_PARSE_PATH = args[0]; }
+		List<String> records = new ArrayList<>()
+		BufferedReader br = new BufferedReader(new FileReader(FILE_TO_PARSE_PATH))
+			String line
+			while ((line = br.readLine()) != null) {
+				String[] values = line.split(",")
+				records.add(Arrays.asList(values))
+			}
 	}
 }
