6f73200bff57d5dff6a9d18641e3fba0cf65efa3
JavaEEPRO <j2eengineer@gmail.com>
2019-11-22 09:47:41 +0200

    Initial commit

---
+++ src/main/groovy/si/inspirited/ConverterApplication.groovy
@@ -0,0 +1,13 @@
+package si.inspirited
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ConverterApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(ConverterApplication, args)
+	}
+
+}
