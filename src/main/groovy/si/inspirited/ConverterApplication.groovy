e5ca3d8d3e3ba67397159fa158c64e5d755a4f74
JavaEEPRO <j2eengineer@gmail.com>
2019-11-22 09:49:30 +0200

    implemented CLR interface

--- src/main/groovy/si/inspirited/ConverterApplication.groovy
+++ src/main/groovy/si/inspirited/ConverterApplication.groovy
@@ -1,13 +1,18 @@
 package si.inspirited
 
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-class ConverterApplication {
+class ConverterApplication implements CommandLineRunner{
 
 	static void main(String[] args) {
 		SpringApplication.run(ConverterApplication, args)
 	}
 
+	@Override
+	void run(String... args) throws Exception {
+
+	}
 }
