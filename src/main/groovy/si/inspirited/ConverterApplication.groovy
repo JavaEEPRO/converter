27ebeed18f1880cd410c69372103e64dcf020b29
JavaEEPRO <j2eengineer@gmail.com>
2019-11-22 11:01:28 +0200

    refactoring: extracted parser logic from run() into the separate method

--- src/main/groovy/si/inspirited/ConverterApplication.groovy
+++ src/main/groovy/si/inspirited/ConverterApplication.groovy
@@ -13,14 +13,19 @@ class ConverterApplication implements CommandLineRunner{
 
 	@Override
 	void run(String... args) throws Exception {
-		String FILE_TO_PARSE_PATH = "D:/input.csv"
-		if (args.length > 0) { FILE_TO_PARSE_PATH = args[0]; }
-		List<String> records = new ArrayList<>()
-		BufferedReader br = new BufferedReader(new FileReader(FILE_TO_PARSE_PATH))
-			String line
-			while ((line = br.readLine()) != null) {
-				String[] values = line.split(",")
-				records.add(Arrays.asList(values))
-			}
-	}
+
+    }
+
+    private List<String> parseCsv(String fileToParsePath) {
+        List<String[]> res = new ArrayList<>()
+        String FILE_TO_PARSE_PATH = "D:/input.csv"
+        if (fileToParsePath != null && fileToParsePath != "") { FILE_TO_PARSE_PATH = fileToParsePath; }
+        BufferedReader br = new BufferedReader(new FileReader(FILE_TO_PARSE_PATH))
+        String line
+        while ((line = br.readLine()) != null) {
+            String[] values = line.split(",")
+            res.add(values)
+        }
+        return res;
+    }
 }
